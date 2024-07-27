51ad5a18a80e126639c1ed61ba315ea54fd12cad
João Vitor Zorzetti <joao.zorzetti@gmail.com>
2024-07-27 00:38:12 -0300

    [WIP] Updated to new name conventions

---
+++ view/services/dao.service.js
@@ -0,0 +1,100 @@
+"use strict";
+
+angular.module("app").factory("DataObjectAccess", [
+    "RuntimeManagerRepository",
+    function (RuntimeManagerRepository) {
+        async function executeFaaS(serviceName, map) {
+            const spark = new Object({
+                params: {
+                    execute: serviceName,
+                },
+                ...(map && {
+                    data: map,
+                }),
+            });
+
+            const execute = await RuntimeManagerRepository.executeFaaS(
+                "DataObjectAccessService",
+                spark
+            );
+
+            return execute.context.response;
+        }
+
+        async function executeContractRule(serviceName, map) {
+            const spark = new Object({
+                execute: serviceName,
+                ...(map
+                    ? {
+                          params: map,
+                      }
+                    : {
+                          params: new Object(),
+                      }),
+            });
+
+            const execute =
+                await RuntimeManagerRepository.executeBusinessRuleWithMap(
+                    "ContractRuleService",
+                    spark
+                );
+
+            return execute.response;
+        }
+
+        async function executeContractTimeline(params) {
+            const spark = new Object({
+                ...(params && Object.keys(params).length
+                    ? {
+                          params: params,
+                      }
+                    : {
+                          params: new Object(),
+                      }),
+            });
+
+            const execute = await RuntimeManagerRepository.executeFlow(
+                "ContractTimelineService",
+                spark
+            );
+
+            return execute.response;
+        }
+
+        async function executeAnalyticalReport(map) {
+            const execute = await RuntimeManagerRepository.executeFlow(
+                "analyticalReport",
+                map
+            );
+            return execute.response;
+        }
+
+        async function executeQuery(_execute, params) {
+            const spark = new Object({
+                execute: _execute,
+                ...(params
+                    ? {
+                          params: params,
+                      }
+                    : {
+                          params: new Object(),
+                      }),
+            });
+
+            const execute = await RuntimeManagerRepository.executeFlow(
+                "executeQuery",
+                spark
+            );
+
+            return execute.response;
+        }
+
+        return {
+            executeFaaS,
+            executeContractRule,
+            executeContractTimeline,
+            executeAnalyticalReport,
+            executeQuery,
+        };
+    },
+]);
---
+++ view/services/repository.service.js
@@ -0,0 +1,113 @@
+"use strict";
+
+const app = angular.module("app");
+
+app.factory("RuntimeManagerRepository", [
+    "$http",
+    "$rootScope",
+    function ($http) {
+        function executeBusinessRule(name, params) {
+            const { $utils } = $rootScope;
+
+            const data = {
+                ruleName: name,
+                data: params,
+            };
+
+            $utils.showLoader();
+
+            return $http
+                .post("/rule", JSON.stringify(data))
+                .then(function (response) {
+                    console.log(
+                        "Resultado executeBusinessRule:",
+                        response.data
+                    );
+
+                    return response.data;
+                })
+                .catch((e) => {
+                    console.log("Erro executeBusinessRule:", e);
+                })
+                .finally(() => {
+                    $utils.hideLoader();
+                });
+        }
+
+        function executeFlow(name, params) {
+            const data = {
+                flowName: name,
+                data: params,
+            };
+
+            $utils.showLoader();
+
+            return $http
+                .post("/flow", JSON.stringify(data))
+                .then(function (response) {
+                    console.log("Resultado executeFlow:", response.data);
+                    return response.data;
+                })
+                .catch((e) => {
+                    console.log("Erro executeBusinessRule:", e);
+                })
+                .finally(() => {
+                    $rootScope.hideLoader();
+                });
+        }
+
+        function executeFaaS(name, params) {
+            const data = {
+                flowName: name,
+                data: params,
+            };
+
+            $rootScope.showLoader();
+
+            return $http
+                .post("/faas", JSON.stringify(data))
+                .then(function (response) {
+                    console.log("FAAS", response.data.context);
+
+                    return response.data.context;
+                })
+                .catch((e) => {
+                    console.log("Erro executeBusinessRule:", e);
+                })
+                .finally(() => {
+                    $rootScope.hideLoader();
+                });
+        }
+
+        function createOrUpdateList(project, businessObject, list) {
+            const data = {
+                project: project,
+                businessObject: businessObject,
+                data: list,
+            };
+
+            $rootScope.showLoader();
+
+            return $http
+                .post("/createOrUpdateList", JSON.stringify(data))
+                .then(function (response) {
+                    console.log("FAAS", response.data.context);
+
+                    return response.data.context;
+                })
+                .catch((e) => {
+                    console.log("Erro executeBusinessRule:", e);
+                })
+                .finally(() => {
+                    $rootScope.hideLoader();
+                });
+        }
+
+        return {
+            executeBusinessRule,
+            executeFlow,
+            executeFaaS,
+            createOrUpdateList
+        }
+    },
+]);
