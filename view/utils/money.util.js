1e59f6a3cdc6f483e30a8ba74581f75cd8135ce5
João Vitor Zorzetti <joao.zorzetti@gmail.com>
2024-07-30 14:33:47 -0300

    Created money util

---
+++ view/utils/money.util.js
@@ -0,0 +1,6 @@
+angular.element(document).ready(function () {
+    angular.module("app").run(['$rootScope', '$filter', ($rootScope, $filter) => {
+        $rootScope.$utils.maskMoney = (valor, sigla, tamanhoFracao) =>
+            $filter("currency")(valor, sigla, tamanhoFracao);
+    }]);
+});
