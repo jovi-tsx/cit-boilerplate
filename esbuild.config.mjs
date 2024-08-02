7792f06fbc5cdac87ebfb9d754a4b7c2ae91819f
João Vitor Zorzetti <joao.zorzetti@gmail.com>
2024-08-02 17:49:36 -0300

    [WIP] Adicionado esbuild ao projeto e implementado o plugin de postcss

---
+++ esbuild.config.mjs
@@ -0,0 +1,35 @@
+import fs from "fs";
+import * as esbuild from "esbuild";
+import tailwindcss from "tailwindcss";
+import postcss from "postcss";
+import postcssPresetEnv from "postcss-preset-env";
+
+await esbuild
+  .build({
+    entryPoints: ["./view/assets/styles/**/*.css"],
+    outdir: ".build/css",
+    bundle: true,
+    plugins: [
+      {
+        name: "postcss",
+        setup(build) {
+          build.onLoad({ filter: /\.css$/ }, async (args) => {
+            const source = fs.readFileSync(args.path, "utf8");
+
+            const { css } = await postcss([
+              tailwindcss,
+              postcssPresetEnv({
+                autoprefixer: {},
+              }),
+            ]).process(source, { from: args.path });
+
+            return {
+              contents: css,
+              loader: "css",
+            };
+          });
+        },
+      },
+    ],
+  })
+  .catch(() => process.exit(1));
