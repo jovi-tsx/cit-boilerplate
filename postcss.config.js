4b56a30f41b7fc243deb1a41d29498f4d1e52cb1
João Vitor Zorzetti <joao.zorzetti@gmail.com>
2024-08-02 18:27:49 -0300

    [1.2.2 - FEAT] Adicionado postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [require("autoprefixer"), require("tailwindcss")],
+  plugins: [
+    require("postcss-nested"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
 };
 
 module.exports = config;
