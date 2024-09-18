6c1ea495467929715acfe210ecf1f5e04dc788f0
João Zorzetti <joao.zorzetti@gmail.com>
2024-09-18 14:26:27 -0300

    Adicionado exclusão de arquivos .json para styles

--- plugins/combine-json.mjs
+++ plugins/combine-json.mjs
@@ -8,7 +8,13 @@ const combineJsonPlugin = (outputFileName) => ({
     build.onLoad({ filter: /\.json$/ }, async (args) => {
       const jsonContent = fs.readFileSync(args.path, "utf8");
       const jsonData = JSON.parse(jsonContent);
+      const filename = args.path.split("\\").slice(-1)[0];
       combinedJson = { ...combinedJson, ...jsonData };
+
+      fs.unlink(args.path, (err) => {
+        if (err)
+          console.log(`❗ Não foi possível deletar o arquivo ${filename}`);
+      });
 
       return {
         contents: jsonContent,
